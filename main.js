d6fcf2313a5943842431549c3cbc0b079241fa08
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-10-27 12:30:05 +0500

    swiper first version

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
+// import Swiper JS
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+// import Swiper styles
+import 'swiper/css';
+
 import { menuToggle } from './src/scripts/menuToggle'
 import './src/styles/style.scss'
 
+const swiper = new Swiper('.reviews__swiper', {
+  modules: [Navigation],
+  slidesPerView: 'auto',
+  // spaceBetween: 30,
+  centeredSlides: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+});
 menuToggle();
