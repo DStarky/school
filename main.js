a1ca23b318c2e771c39c6d90b3d412b360c12c60
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-10-28 07:30:49 +0500

    slider done

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // import Swiper JS
 import Swiper from 'swiper';
 import { Navigation } from 'swiper/modules';
-// import Swiper styles
+// import Swiwper styles
 import 'swiper/css';
 
 import { menuToggle } from './src/scripts/menuToggle'
@@ -10,12 +10,18 @@ import './src/styles/style.scss'
 const swiper = new Swiper('.reviews__swiper', {
   modules: [Navigation],
   slidesPerView: 'auto',
+  initialSlide: 1,
   // spaceBetween: 30,
   centeredSlides: true,
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+  breakpoints: {
+    1200: {
+      spaceBetween: 100,
+    }
+  }
 
 });
 menuToggle();
