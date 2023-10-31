82e4e2be7c16af231602475317a2028cfc7fd594
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-10-31 09:26:30 +0500

    add before element for FAQ

--- src/scripts/collapse.js
+++ src/scripts/collapse.js
@@ -1,17 +1,24 @@
 export const collapse = () => {
   const faq = document.querySelector('.faq');
   const questions = document.querySelectorAll('.faq__title')
+  const collapses = document.querySelectorAll('.faq__question');
 
   faq.addEventListener('click', (e) => {
     const { target } = e;
     if (target.classList.contains('faq__title')) {
       if (target.classList.contains('active')) {
         target.classList.remove('active');
+        target.closest('.faq__question').classList.remove('active');
+
       } else {
         questions.forEach(question => {
           question.classList.remove('active')
         })
+        collapses.forEach(question => {
+          question.classList.remove('active')
+        })
         target.classList.add('active');
+        target.closest('.faq__question').classList.add('active');
       }
     }
   })
